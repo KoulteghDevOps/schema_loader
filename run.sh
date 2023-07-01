c006733e19509fb8d602529ddcf66bdc8484cdbe
KoulteghDevOps <koultegh@outlook.com>
2023-07-01 22:35:25 +0100

    manifest updated

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
-git clone https://github.com/KoulteghDevOps/${COMPONENT}
+#git clone https://github.com/KoulteghDevOps/${COMPONENT}
+#cd $COMPONENT
+#
+#if [ "$SCHEMA_TYPE" == "mongo" ]; then
+#  DOCDB_ENDPOINT=$(aws ssm get-parameters  --names prod.docdb.db_endpoint --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+#  DOCDB_USER=$(aws ssm get-parameters  --names prod.docdb.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+#  DOCDB_PASS=$(aws ssm get-parameters  --names prod.docdb.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+#  curl -L https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem -o rds-combined-ca-bundle.pem
+#  mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} <schema/${COMPONENT}.js
+#
+#fi
+#
+#if [ "$SCHEMA_TYPE" == "mysql" ]; then
+#echo
+#
+#fi
+
+git clone https://github.com/raghudevopsb72/${COMPONENT}
 cd $COMPONENT
 
 if [ "$SCHEMA_TYPE" == "mongo" ]; then
@@ -11,6 +28,9 @@ if [ "$SCHEMA_TYPE" == "mongo" ]; then
 fi
 
 if [ "$SCHEMA_TYPE" == "mysql" ]; then
-echo
 
+  RDS_ENDPOINT=$(aws ssm get-parameters  --names prod.rds.db_host --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  RDS_USER=$(aws ssm get-parameters  --names prod.rds.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  RDS_PASS=$(aws ssm get-parameters  --names prod.rds.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  mysql -h ${RDS_ENDPOINT}  -u${RDS_USER} -p${RDS_PASS} < schema/${COMPONENT}.sql
 fi
